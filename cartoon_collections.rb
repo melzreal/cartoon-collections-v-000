a25400dcd3495ddc0a870898a5049bddbe255ec1
Melissa Real <melissa.real@hotmail.com>
2018-04-26 17:33:14 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,8 +16,9 @@ def summon_captain_planet(cartoons)
   
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  
+
 end
 
 def find_the_cheese# code an argument here
