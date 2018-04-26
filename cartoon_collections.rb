8ad79dad9843707dc8ac0c528c0f24633506dda3
Melissa Real <melissa.real@hotmail.com>
2018-04-26 18:56:27 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,11 +1,9 @@
 def roll_call_dwarves(dwarves)
 
   dwarves_arr = []
-
     dwarves.each.with_index(1) do |dwarf, i|
       dwarves_arr << " #{i} #{dwarf}"
     end
-    
   puts dwarves_arr
   
 end
@@ -19,10 +17,11 @@ end
 def long_planeteer_calls(calls)
     
  len = calls.detect{|check| check.length > 4 ? true : false} 
-    
  len != nil  ? true : false 
     
 end
+
+
 
 def find_the_cheese# code an argument here
   # the array below is here to help
